1f9d70b53e8060316cdc3b6548e8e5c2d11eb1ed
LVjiahao <LVjiahao@qq.com>
2022-10-05 11:32:57 +0800

    hot-fix commit

--- src/main/java/com/atuigu/git/TestGit.java
+++ src/main/java/com/atuigu/git/TestGit.java
@@ -3,5 +3,6 @@ package com.atuigu.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,hot-fix");
     }
 }
